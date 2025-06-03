4527c82e3ce13f74f02dd8028c2de68500de8c73
Md. Mehraj Hossain Akash <mehrajakash81@gmail.com>
2025-06-03 23:07:21 +0600

    dp

--- Dynamic Programming/Dice combination.cpp
+++ Dynamic Programming/Dice combination.cpp
@@ -2,25 +2,31 @@
 using namespace std;
 
 int n;
+int dp[1000000+5],vis[1000000+5];
 
 int solve(int sum){
 
-	cout<<"Sum now "<<sum<<endl;
+	//cout<<"Sum now "<<sum<<endl;
 
 	if(sum==0) return 1;
+
+	if(vis[sum]==1) return dp[sum];
 
     int res=0;
 
 	for(int j=1; j<=6; j++){
 
-             cout<<"RESult now "<<res<<endl;
-
-
 		if(sum>=j){
+
 			res += solve(sum-j);
 		}
 
 	}
+
+	   cout<<"sum: "<<sum<<"  res: "<<res<<endl;
+
+	   vis[sum] = 1;
+	   dp[sum] = res;
 
 	return res;
 }
@@ -28,7 +34,31 @@ int solve(int sum){
 
 int main() {
 
-	cin>>n;
+    int sum; cin>>sum;
 
-	cout<<solve(3)<<endl;
+	cout<<solve(sum)<<endl; /// time Complexity: 6^sum
 }
+/// problem link: https://cses.fi/problemset/task/1633
+
+/**
+Your task is to count the number of ways to construct sum n by throwing a dice one or more times. Each throw produces an outcome between 1 and  6.
+For example, if n=3, there are 4 ways:
+
+1+1+1
+1+2
+2+1
+3
+
+Input
+The only input line has an integer n.
+Output
+Print the number of ways modulo 10^9+7.
+
+Example
+
+Input:
+3
+
+Output:
+4
+**/
