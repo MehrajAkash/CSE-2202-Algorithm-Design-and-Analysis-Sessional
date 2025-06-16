3a2e9dc6be484d6e2f7684d56a80713e15850765
Md. Mehraj Hossain Akash <mehrajakash81@gmail.com>
2025-06-17 00:16:28 +0600

    Coin Change Algo

--- Dynamic Programming/Coin Change/Minimum Coins to make Sum.cpp
+++ Dynamic Programming/Coin Change/Minimum Coins to make Sum.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int>coins = {3,1,5};
 int n, vis[10000005], dp[10000005];
 
 int solve(int m){
+
 	if(m==0) return 0;
 	if(vis[m]) return dp[m];
-	int m2=m;
+
 	int res=INT_MAX;
 
-	while(m2>0){
-		int digit=m2%10;
-		if(digit!=0){
-			res=min(res,1+solve(m-digit));
-		}
-		m2/=10;
+	for(int i=0; i<coins.size(); i++){
+
+		res=min( res, 1+solve(m-coins[i]) );
+
 	}
 	vis[m]=1;
 	dp[m]=res;
@@ -23,16 +23,19 @@ int solve(int m){
 }
 
 int main(){
+
 	cin>>n;
 	int ans=solve(n);
-	cout<<ans<<'\n';
+	cout<<endl<<ans<<'\n';
 	return 0;
 }
 
-/***
-input: 12
-output: 3
+/**
+input: 8
+output: 2
 
-explaination: 12 -> (12-2) 10 -> (10-1) 9 -> (9-9) 0   here number of steps are 3.
+explaination: 5+3 = 8 , here two coins needed making sum 8
 
 **/
+
+
