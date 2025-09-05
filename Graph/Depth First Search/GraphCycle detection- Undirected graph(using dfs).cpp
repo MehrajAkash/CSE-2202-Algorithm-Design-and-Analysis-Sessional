d1d69a02fd6effe6f2cc5539b3d22293d348d741
Md. Mehraj Hossain Akash <mehrajakash81@gmail.com>
2025-09-05 18:05:33 +0600

    DFS & Backtracking

---
+++ Graph/Depth First Search/GraphCycle detection- Undirected graph(using dfs).cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool detectCycle(vector<vector<int>> &adj, int u, vector<bool>&visited, vector<bool> &record){
+
+    //If the node is already in the recursion stack, a cycle is detected
+    if(record[u])
+        return true;
+
+    // If the node is already visited and not in recursion stack, no need to check again
+    if(visited[u])
+        return false;
+
+    visited[u] = true;
+    record[u] = true;
+
+    // traverse for all neighbors
+    for(int x : adj[u]){
+
+        if(detectCycle(adj, x, visited, record))
+            return true;
+    }
+    // remove the node from the recursion stack
+    record[u] = false;
+    return false;
+}
+
+bool isCyclic(int v, vector<vector<int>> &edges){
+
+    // creating adjacency list
+    vector<vector<int>> adj(v);
+    for(auto it : edges){
+        adj[it[0]].push_back(it[1]);
+    }
+
+    vector<bool>visited(v, false);
+    vector<bool>record(v, false);
+
+    // Check for cycles starting from every unvisited node
+    for(int i=0; i<v; i++){
+
+        if( !visited[i] && detectCycle( adj, i, visited, record ) )
+            return true; // cycle found
+
+    }
+
+    return false;
+}
+
+int main(){
+
+    int v=4;
+
+    vector<vector<int>> edges={ {0, 1}, {0, 2}, {1, 2}, {2, 0}, {2, 3} };
+
+    cout<< (isCyclic(v, edges) ? "true cycle-detected" : "false no-cycle") <<endl;
+
+return 0;
+}
